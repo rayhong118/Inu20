1e4d65121f25e5d9709ca962237fc409f1d168c9
Zhihao Hong <rayhong118@gmail.com>
2019-09-29 21:37:50 -0700

    reorder items UI only

--- src/pages/restaurants/restaurant-page.js
+++ src/pages/restaurants/restaurant-page.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import {Container, Message, Icon} from 'semantic-ui-react'
+import {Container, Message, Icon, Select} from 'semantic-ui-react'
 import ItemsList from './restaurant-list';
 import ItemModal from './restaurant-modal';
 import { connect } from 'react-redux';
 import { firestoreConnect} from 'react-redux-firebase';
 import { compose } from 'redux';
 
+const listOrder = [
+  {key: 'Default', value: 'Default', text:'Default'},
+  {key: 'PL2H', value: 'PL2H', text:'Price: Low to high'},
+  {key: 'PH2L', value: 'PH2L', text:'Price: High to low'}
+]
+
 class Restautant extends React.Component {
+  state = {order: null}
+  handleChange = (e) => {
+    this.setState({order:e.target.value});
+  }
+  
   render() {
     return(
-        <Container>
-          <ItemModal item={ {} } type={'add'}></ItemModal>
-          {this.props.items ? (
-            <div>Total number of items: {this.props.items.length}
-              <ItemsList items={this.props.items}/>
-            </div>
-          ) : (
-            <Message color='yellow'>
-              <Icon name='circle notch' loading={true}></Icon>
-              loading...
-            </Message>
-          )}
-        </Container>
+      <Container>
+        <ItemModal item={ {} } type={'add'}></ItemModal>
+        <Select placeholder='Order items' options={listOrder} value={this.state.order} onChange={this.handleChange}/>
+        {this.props.items ? (
+          <div>Total number of items: {this.props.items.length}
+            <ItemsList items={this.props.items} order={this.state.order}/>
+          </div>
+        ) : (
+          <Message color='yellow'>
+            <Icon name='circle notch' loading={true}></Icon>
+            loading...
+          </Message>
+        )}
+      </Container>
     )
   }
     
