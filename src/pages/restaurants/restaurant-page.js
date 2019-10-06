5fbf37e802ab68816a1de06daa323c029ae7f8c9
Zhihao Hong <rayhong118@gmail.com>
2019-10-05 18:18:17 -0700

    code clean up

--- src/pages/restaurants/restaurant-page.js
+++ src/pages/restaurants/restaurant-page.js
@@ -31,9 +31,7 @@ class Restautant extends React.Component {
         break;
       default:
         this.props.items.sort((a, b) => {
-          if(a.name < b.name) { return -1; }
-          if(a.name > b.name) { return 1; }
-          return 0;
+          return a.name.localeCompare(b.name);
         });
         break;
     }
