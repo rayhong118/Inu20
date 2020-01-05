dbbb92f6a27268c8fe5568428eb0761f070ac74c
Zhihao Hong <rayhong118@gmail.com>
2020-01-04 17:06:00 -0800

    add expand on hover panel to main page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import './home.css'
 
 export default class HomePage extends Component{
   render() {
 
     return(
-      <div>
-        <p>
-          home page
-        </p>
+      <div className="img-container">
+        <div className="img">1</div>
+        <div className="img">2</div>
+        <div className="img">3</div>
+        <div className="img">4</div>
+        <div className="img">5</div>
+
 
       </div>
     )
