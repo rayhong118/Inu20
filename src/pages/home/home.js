7446601b72d70e2627ebf07eab0487cb9a72c4da
Zhihao Hong <rayhong118@gmail.com>
2020-01-13 21:11:27 -0800

    a bit code clean up

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import './home.css'
 
 export default class HomePage extends Component{
+  displayContent(e){
+    console.log('transition end');
+    console.log(e.target);
+    e.target.classList.toggle('hidden');
+  }
+
   render() {
 
     return(
-      <div className="img-container">
-        <div className="img">1</div>
+      <div className="img-container" 
+      onTransitionEnd={(e)=>this.displayContent(e)} >
+        <div className="img hidden">1
+          <div>
+            this should display when not being hovered
+
+            this should be hidden when not being hovered
+          </div>
+        </div>
         <div className="img">2</div>
         <div className="img">3</div>
         <div className="img">4</div>
         <div className="img">5</div>
-
 
       </div>
     )
