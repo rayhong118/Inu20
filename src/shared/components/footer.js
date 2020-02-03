39f7b407650b168a36e49acabbef6491890acef2
Zhihao Hong <rayhong118@gmail.com>
2020-02-02 19:50:00 -0800

    style fixes

--- src/shared/components/footer.js
+++ src/shared/components/footer.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './footer.css';
+import { Container, Icon } from 'semantic-ui-react';
 
 export default class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
-        <img className='footer-icon' src='' />
-        <div>
-          email: rayhong118@gmail.com <br />
-          GitHub: dogheadslug
-        </div>
+        <Container>
+          <div className='footer-content'>
+            <img className='footer-icon' src='https://i.imgur.com/1o29Oyu.jpg' />
+            <span>
+              <Icon name='mail' />
+              rayhong118@gmail.com <br />
+              <a href='https://github.com/dogheadslug'>
+                <Icon name='github' />
+                dogheadslug
+              </a>
+            </span>
+          </div>
+        </Container>
       </div>
     );
   }
