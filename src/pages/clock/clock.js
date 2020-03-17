8fb5f700b7c11b0bef096a69eef106b7a61dadee
Zhihao Hong <rayhong118@gmail.com>
2020-03-17 15:08:34 -0700

    more style update

--- src/pages/clock/clock.js
+++ src/pages/clock/clock.js
@@ -36,22 +36,37 @@ export default class Clock extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className='evaClockContainer'>
-          <div>
-            <h1 className='evaClockPlaceholder'>88:88:88:88</h1>
-            <h1 className='evaClock'>{this.state.time}</h1>
-          </div>
-          <div className='clockStatusSelect'>
-            <div className='clockStatusTitle'></div>
-            <h4 className='clockStatusContent'>测试</h4>
-          </div>
-
-          <div className='clockStatusSelect'>
-            <div className='clockStatusTitle'></div>
-            <h4 className='clockStatusContent'>计时</h4>
-          </div>
+      <div className='evaClockContainer'>
+        <div className='clockStatusSelect'>
+          <div className='clockStatusTitle'></div>
+          <h4 className='clockStatusContent'>測試</h4>
         </div>
+
+        <div className='clockStatusSelect'>
+          <div className='clockStatusTitle'></div>
+          <h4 className='clockStatusContent'>計時</h4>
+        </div>
+
+        <div>
+          <h1 className='evaClockPlaceholder'>88:88:88:88</h1>
+          <h1 className='evaClock'>{this.state.time}</h1>
+        </div>
+
+        <input className='eva-input' />
+        <label className='eva-label'>
+          <div>分</div>
+          <div>MIN</div>
+        </label>
+        <input className='eva-input' />
+        <label className='eva-label'>
+          <div>秒</div>
+          <div>SEC</div>
+        </label>
+
+        <button className='eva-button'>
+          <span>countdown</span>
+          <span>倒計時</span>
+        </button>
       </div>
     );
   }
