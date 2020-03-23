c2b9c7e3743fa1b5a18e5952dfeb0b26aa94f683
Zhihao Hong <rayhong118@gmail.com>
2020-03-22 21:08:01 -0700

    form validation added to restaurant form modal

--- src/pages/restaurants/restaurant-modal.js
+++ src/pages/restaurants/restaurant-modal.js
@@ -11,17 +11,19 @@ class ItemModal extends React.Component {
   state = {
     modalOpen: false,
     item: this.props.item,
+    isFormValid: false,
   };
 
   handleChange = e => {
     let elem = e.target;
     if (elem.id === 'price' && elem.value <= 0) elem.value = 0;
-    this.setState({
-      item: {
-        ...this.state.item,
-        [e.target.id]: e.target.value,
-      },
-    });
+
+    let item = {
+      ...this.state.item,
+      [e.target.id]: e.target.value,
+    };
+
+    this.validateForm(item);
   };
 
   openModal = () => {
@@ -66,9 +68,16 @@ class ItemModal extends React.Component {
     service.getDetails(request, (place, status) => {
       if (status === window.google.maps.places.PlacesServiceStatus.OK) {
         console.log(place.name, place.formatted_address);
-        this.setState({ item: { name: place.name, address: place.formatted_address } });
+        let item = { name: place.name, address: place.formatted_address };
+        this.validateForm(item);
       }
     });
+  };
+
+  validateForm = item => {
+    const isValid = item.name && item.address && !!parseInt(item.price);
+    this.setState({ isFormValid: isValid, item });
+    console.log(this.state);
   };
 
   render() {
@@ -82,7 +91,11 @@ class ItemModal extends React.Component {
 
     if (this.props.type === 'add') {
       actionButton = (
-        <Button type='button' color={action.add.color} onClick={this.addData}>
+        <Button
+          type='button'
+          color={action.add.color}
+          onClick={this.addData}
+          disabled={!this.state.isFormValid}>
           Add
         </Button>
       );
@@ -94,7 +107,11 @@ class ItemModal extends React.Component {
       );
     } else if (this.props.type === 'edit') {
       actionButton = (
-        <Button type='button' color={action.edit.color} onClick={this.editData}>
+        <Button
+          type='button'
+          color={action.edit.color}
+          onClick={this.editData}
+          disabled={!this.state.isFormValid}>
           Edit
         </Button>
       );
@@ -121,7 +138,7 @@ class ItemModal extends React.Component {
         <Modal.Content>
           <Form>
             <Form.Group>
-              <Form.Field width={16}>
+              <Form.Field width={16} required readOnly={this.props.type === 'delete'}>
                 <label>Add Place by Google Map Link</label>
                 <Input
                   defaultValue={this.props.item.mapLink}
@@ -136,20 +153,31 @@ class ItemModal extends React.Component {
                   }
                 />
               </Form.Field>
-              <Form.Field width={8}>
-                <label>Name:</label>
-                <Input value={this.state.item.name || ''} />
-              </Form.Field>
-              <Form.Field width={8}>
-                <label>Address:</label>
-                <Input value={this.state.item.address || ''} />
-              </Form.Field>
 
+              <Form.Field
+                label='Name:'
+                control='input'
+                value={this.state.item.name || ''}
+                type='text'
+                id='name'
+                readOnly
+                width={6}
+              />
+
+              <Form.Field
+                label='Address:'
+                control='input'
+                value={this.state.item.address || ''}
+                type='text'
+                id='address'
+                readOnly
+                width={10}
+              />
               <Form.Field
                 label='Price'
                 control='input'
-                defaultValue={this.props.item.price}
-                onBlur={this.handleChange}
+                defaultValue={this.props.item.price || 0}
+                onChange={this.handleChange}
                 type='number'
                 id='price'
                 readOnly={this.props.type === 'delete'}
@@ -159,7 +187,7 @@ class ItemModal extends React.Component {
             <Form.TextArea
               label='Comments ( additional info )'
               defaultValue={this.props.item.comments}
-              onBlur={this.handleChange}
+              onChange={this.handleChange}
               id='comments'
               readOnly={this.props.type === 'delete'}
             />
