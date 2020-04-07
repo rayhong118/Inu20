1a09f21fb0bea43508c4602fbe66c5fe6330c944
Zhihao Hong <rayhong118@gmail.com>
2020-04-06 22:15:36 -0700

    implemented basic countdown

--- src/pages/clock/clock.js
+++ src/pages/clock/clock.js
@@ -2,22 +2,28 @@ import React from 'react';
 import './clock.css';
 
 export default class Clock extends React.Component {
-  timeUpdate;
+  timeUpdateInterval;
+
   state = {
     isCountdownEnded: true,
+    thenTime: null,
   };
 
-  handleInput = e => {
+  handleInput = (e) => {
     let elem = e.target;
-    elem.value = Math.max(0, elem.value);
+    elem.value = Math.max(0, parseInt(elem.value) || 0);
     this.setState({
       [e.target.id]: e.target.value,
     });
   };
 
   componentDidMount() {
-    this.timeUpdate = setInterval(() => {
-      let date = new Date();
+    this.enterClockMode();
+  }
+
+  enterClockMode = () => {
+    this.timeUpdateInterval = setInterval(() => {
+      const date = new Date();
       let h = date.getHours();
       let m = date.getMinutes();
       let s = date.getSeconds();
@@ -25,10 +31,34 @@ export default class Clock extends React.Component {
 
       this.setState({ time: this.formatTime(h, m, s, cs) });
     }, 43);
-  }
+  };
+
+  enterCountdownMode = () => {
+    clearInterval(this.timeUpdateInterval);
+    console.log(this.state);
+    const now = Date.now();
+    const min = this.state.min || 0;
+    const sec = this.state.sec || 0;
+    const then = now + (min * 60 + sec) * 1000;
+    console.log(now, then);
+    const countdownInterval = setInterval(() => {
+      const timeLeft = then - Date.now();
+      const cs = Math.floor((timeLeft % 1000) / 10);
+      const s = Math.floor(timeLeft / 1000);
+      const m = Math.floor(timeLeft / 1000 / 60);
+      const h = Math.floor(timeLeft / 1000 / 60 / 60);
+
+      this.setState({ time: this.formatTime(h, m, s, cs) });
+      if (timeLeft < 0) {
+        clearInterval(countdownInterval);
+        this.setState({ time: this.formatTime(0, 0, 0, 0) });
+        return;
+      }
+    }, 43);
+  };
 
   componentWillUnmount() {
-    clearInterval(this.timeUpdate);
+    clearInterval(this.timeUpdateInterval);
   }
 
   formatTime(h, m, s, cs) {
@@ -57,20 +87,24 @@ export default class Clock extends React.Component {
           <h1 className='evaClock'>{this.state.time}</h1>
         </div>
 
-        <input className='eva-input' />
+        <input id='min' className='eva-input' onChange={this.handleInput} />
         <label className='eva-label'>
           <div>分</div>
           <div>MIN</div>
         </label>
-        <input className='eva-input' />
+        <input id='sec' className='eva-input' onChange={this.handleInput} />
         <label className='eva-label'>
           <div>秒</div>
           <div>SEC</div>
         </label>
 
-        <button className='eva-button'>
+        <button className='eva-button' onClick={this.enterCountdownMode}>
           <span>countdown</span>
           <span>倒計時</span>
+        </button>
+
+        <button className='eva-button' onClick={this.enterClockMode}>
+          clock
         </button>
       </div>
     );
