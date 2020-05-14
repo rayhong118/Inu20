3ad6cd572d223557794cb093c80c2816118b1101
Zhihao Hong <rayhong118@gmail.com>
2020-05-13 23:49:46 -0700

    firebase function for deleteing restaurant tags

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,54 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.updateTags = functions.firestore
+  .document('/restaurants/{id}')
+  .onWrite((snapshot, context) => {
+    const oldValue = snapshot.before.data();
+    const newValue = snapshot.after.data();
+    const oldTags = oldValue.tags || [];
+    const newTags = newValue.tags || [];
+    const tagsAdded = newTags.filter((tag) => !oldTags.includes(tag));
+    const tagsRemoved = oldTags.filter((tag) => !newTags.includes(tag));
+
+    const tagsCollectionRef = admin.firestore().collection('restaurant-tags');
+
+    let batch = admin.firestore().batch();
+
+    let promiseAddTags = tagsAdded.forEach((tag) => {
+      let id = tag.toLowerCase();
+      tagsCollectionRef
+        .where('id', '==', id)
+        .get()
+        .then((querySnapshot) => {
+          if (querySnapshot.empty) {
+            let newTagRef = tagsCollectionRef.doc();
+            batch.set(newTagRef, { id, text: tag, count: 1 });
+          } else {
+            let result = querySnapshot.docs[0];
+            batch.update(result.ref, { count: result.data().count + 1 });
+          }
+        });
+    });
+
+    let promiseRemoveTags = tagsRemoved.length
+      ? tagsCollectionRef
+          .where('id', 'in', tagsRemoved)
+          .get()
+          .then((querySnapshot) => {
+            if (querySnapshot.empty) return null;
+            else {
+              querySnapshot.forEach((doc) => {
+                batch.update(doc.ref, {
+                  count: doc.data().count && doc.data().count - 1,
+                });
+              });
+            }
+          })
+      : null;
+
+    return Promise.all([promiseAddTags, promiseRemoveTags]).then(() => {
+      return batch.commit();
+    });
+  });
